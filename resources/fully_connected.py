17e70e0ad6c355f994f0597b4bc8adadeb8e7869
mendelyuval9@gmail.com <mendelyuval9@gmail.com>
2025-01-14 19:46:24 +0200

    First commit to project

--- resources/fully_connected.py
+++ resources/fully_connected.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+#This file traines a fully connected Nural Network
+
+class Network:
+    def __init__(self, sizes):
+        """This Function initializes the network.
+           sizes is a list that contains the size of each layer
+        """
+        pass
+        
+class Layer:
+    def __init__(self, prev_layer_size):
+        """This Function initiaizes a layer.
+        this class will be used Only inside network.
+        prev_layer_size is the length of the previous layer, needed for the weights matrix
+        """
+        pass
