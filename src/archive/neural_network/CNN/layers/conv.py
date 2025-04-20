f5297c54767c88c80a6b825215097c237958cbff
mendelyuval9@gmail.com <mendelyuval9@gmail.com>
2025-04-20 23:07:22 +0300

    implemented init, need to implement forward and backward

--- src/archive/neural_network/CNN/layers/conv.py
+++ src/archive/neural_network/CNN/layers/conv.py
@@ -1,5 +1,53 @@
 import numpy as np
 from base import TrainableLayer
 
+
 class Conv2D(TrainableLayer):
-    pass
+    def __init__(self, in_channels, out_channels, kernel_size, initialization, stride=1, padding=0):
+        """Initializes a Conv2D layer with weights and biases.
+           - in_channels: Number of input channels.
+           - out_channels: Number of output channels (filters).
+           - kernel_size: Size of the convolutional kernel (height, width).
+           - initialization: Function to initialize weights.
+           - stride: Stride of the convolution.
+           - padding: Padding added to the input.
+        """
+        super().__init__()
+        kh, kw = kernel_size
+
+        #  The biases have the shape (1, out_channels) (one bias per filter)
+        self.biases = np.zeros((1, out_channels))
+
+        #  The Weights have the shape (out_channels, in_channels, kh, kw)
+        #  for each filter, we have (in_channels * kh * kw) weights (the shape of the kernel)
+        self.weights = initialization(out_channels, in_channels, kh, kw)
+
+        self.weights_gradient = np.zeros_like(self.weights)
+        self.biases_gradient = np.zeros_like(self.biases)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.stride = stride
+        self.kernel_size = (kh, kw)
+
+        self.padding = padding
+
+        self.input_data = None
+        self.output = None
+        self.output_shape = None
+
+    def forward(self, input_data):
+        """Computes the convolution operation.
+           - input_data: The input from the previous layer.
+           - Saves calculation and returns it.
+        """
+        pass
+
+    def backward(self, output_grad):
+        """Computes the gradient of the loss with respect to the input.
+           - output_grad: The gradient of the loss with respect to the output.
+           - Returns the gradient of the loss with respect to the input.
+        """
+        pass
+
